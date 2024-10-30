8ec3807f212f56180a1b9f7b6e609f30c822eb9f
Ahmad0301 <sehroz3040@gmail.com>
2024-10-30 18:42:54 +0500

    make get put delete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9000;
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); 
 app.listen(port, () => {
   console.log(`Server is running `);
 });
-app.get('/book', (req, res) => {
-    res.send('Welcome to the Book API');
-  });
+app.get('/', (req, res) => {
+  res.send('welcome to the book API!');
+});
+
+const books = [
+  { id: 1, title: 'Sample Book', author: 'Author Name', year: 2022 }
+];
+
+app.get('/post', (req, res) => {
+  res.send(books);
+});
+
+app.post('/post', (req, res) => {
+  res.send('Book added ');
+});
+
+app.put('/post', (req, res) => {
+  res.send('Book updated');
+});
+
+app.delete('/post', (req, res) => {
+  res.send('Book deleted');
+});
+
+
+
+
